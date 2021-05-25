3a5df5c7f3affb5a05645fc3cc6f8b6385b64d88
Motti Wilschanski <mchillywilly@gmail.com>
2021-05-25 22:51:33 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,17 +6,25 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+<<<<<<< HEAD
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
 def valid_move?(board, index)
   if index.between?(0,8)
+=======
+def valid_move?(board, index)
+  if index.between?(1, 9)
+>>>>>>> f8fe708aa4862b2d61bfe2f27ef3f271d4a7bdf8
     if !position_taken?(board, index)
       true
     end
   end
+<<<<<<< HEAD
 
   def move(board, index, current_player = "X")
     board[index] = current_player
   end
+=======
+>>>>>>> f8fe708aa4862b2d61bfe2f27ef3f271d4a7bdf8
